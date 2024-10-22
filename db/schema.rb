5b54d2d453e73fd52f1f83854d3ac2b5c2c303bd
heidless <lockhart.r@gmail.com>
2024-10-22 21:10:25 +0100

    deploy updates

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_30_121831) do
   end
 
   create_table "articles", force: :cascade do |t|
-    t.string "title"
+    t.text "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -36,11 +36,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_30_121831) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
+    t.text "username"
+    t.text "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
+    t.text "password_digest"
     t.boolean "admin", default: false
   end
 
